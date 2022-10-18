f13636e95f0caf6f114b2878c904b70709a25764
kevinolweb <kevin.oleary91@gmail.com>
2022-10-18 15:04:13 +0000

    Javascript amends to display more questions

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -9,7 +9,18 @@ const quizData = [
         second_highest_score:"a",
         third_highest_score:"c",
         fourth_highest_score:"d"
-    }
+    },
+    {
+        question:"Are you good at Maths and numerics/statistics?",
+        answer_a:"I hate Maths and statistics ",
+        answer_b:"I am relatively good at Maths but enjoy more creative pursuits",
+        answer_c:"I enjoy Maths and never forget a date ",
+        answer_d:"Maths and statistics come easy to me and I love to open up an excel sheet",
+        highest_score:"d",
+        second_highest_score:"c",
+        third_highest_score:"b",
+        fourth_highest_score:"a"
+    },
 ]
 
 let question_text = document.getElementById("quiz-question")
@@ -17,7 +28,7 @@ let a_text = document.getElementById("a-label")
 let b_text = document.getElementById("b-label")
 let c_text = document.getElementById("c-label")
 let d_text = document.getElementById("d-label")
-
+let nextbtn = document.getElementById("next-btn")
 
 let currentQuestion = 0;
 let score = 0;
@@ -26,6 +37,7 @@ document.addEventListener("DOMContentLoaded",runGame)
 
 function runGame(){
     displayQuestion()
+    
 }
 
 function displayQuestion(){
@@ -34,4 +46,13 @@ function displayQuestion(){
     b_text.innerText = quizData[currentQuestion].answer_b
     c_text.innerText = quizData[currentQuestion].answer_c
     d_text.innerText = quizData[currentQuestion].answer_d
+    
 }
+
+nextbtn.addEventListener("click", function(){
+    currentQuestion++
+    if (currentQuestion<quizData.length){
+        runGame()
+    }
+})
+    
