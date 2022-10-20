b79daee25efe8a0d6b308001488a3f8f24a2be3e
kevinolweb <kevin.oleary91@gmail.com>
2022-10-20 15:51:42 +0000

    All questions added to js and scoring system updated

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -42,10 +42,10 @@ const quizData = [
     {
         question:"What kind of job best suits you?",
         answers:{
-            answer_a:"I prefer a job where I can learn what to do and does not require further study",
-            answer_b:"I love a job where I do have all the answers and have to learn new skills every day at work or after work",
-            answer_c:"I like to learn but think I should be paid by my employer to upskill in which case I would be happy to further study",
-            answer_d:"I want a job which is challenging and sometimes requires me to improve my skillset through outside learning ",
+            answer_a:"I prefer a job which does not require further study",
+            answer_b:"I love a job where I do not have all the answers and have to learn new skills",
+            answer_c:"I like to learn but further learning should be paid by my employer",
+            answer_d:"I want a job which is challenging and requires continual learning",
         },
         highest_score:"b",
         second_highest_score:"d",
@@ -64,6 +64,71 @@ const quizData = [
         second_highest_score:"d",
         third_highest_score:"b",
         fourth_highest_score:"c"
+    },
+    {
+        question:"Which kind of work environment best suits you?",
+        answers:{
+            answer_a:"I like to work on my own initiative",
+            answer_b:"I prefer to be told what exactly I need to do",
+            answer_c:"I like regular colleague support to complete a task",
+            answer_d:"I liaise with my team and manager to get work done",
+        },
+        highest_score:"a",
+        second_highest_score:"d",
+        third_highest_score:"c",
+        fourth_highest_score:"b"
+    },
+    {
+        question:"What would your dream job entail?",
+        answers:{
+            answer_a:"Managing projects and meeting clients",
+            answer_b:"Delivering presentations and selling products/services",
+            answer_c:"Sitting behind a screen coding and solving bugs/problems",
+            answer_d:"Chatting to customers and helping solve their problems",
+        },
+        highest_score:"c",
+        second_highest_score:"a",
+        third_highest_score:"b",
+        fourth_highest_score:"d"
+    },
+    {
+        question:"What is your attitude to stress?",
+        answers:{
+            answer_a:"I get stressed easily and prefer a low stress role",
+            answer_b:"I don’t mind a little stress ",
+            answer_c:"The stress of an upcoming deadline motivates me",
+            answer_d:"I thrive in stressful environments",
+        },
+        highest_score:"d",
+        second_highest_score:"c",
+        third_highest_score:"b",
+        fourth_highest_score:"a"
+    },
+    {
+        question:"Please select the option below which best represents you:",
+        answers:{
+            answer_a:"I am a real people person",
+            answer_b:"I love gadgets, technology and am a bit of a 'nerd'",
+            answer_c:"I love intellectual and creative outlets",
+            answer_d:"I have a wide range of hobbies",
+        },
+        highest_score:"b",
+        second_highest_score:"c",
+        third_highest_score:"a",
+        fourth_highest_score:"d"
+    },
+    {
+        question:"A colleague has a problem with the laptop, how would you help:",
+        answers:{
+            answer_a:"I would ping them an email with the user manual",
+            answer_b:"I would give them a call to find out more",
+            answer_c:"I’d call over to their desk and ask if they need my help",
+            answer_d:"I’d have a quick look online to find anything to help them",
+        },
+        highest_score:"c",
+        second_highest_score:"d",
+        third_highest_score:"b",
+        fourth_highest_score:"a"
     },
 ];
 
@@ -171,11 +236,17 @@ function calculateScore(theAnswer){
 
 function showResult(){
     let resultText;
-    if (score>15){
-        resultText = "You can be a great developer";
+    if (score>=80){
+        resultText = "You are stronly suited to a technical role based on your answers. Careers like a software developer, Data Scientist, stack engineer, App developer etc. could await you. Get started on the coding journey today!";
     }
-    else if(score<15){
-        resultText = "You can be a great  just not a dev!";
+    else if(score>=60 && score<80){
+        resultText = "Your answers indicate that you could be well suited to a technical role which also allows for creativity and a lot of collaboration. Careers like a Web Designer, Front end developer, systems architect, IT administrator etc. could await you. Get started on the coding journey today!";
+    }
+    else if(score>=40 && score<60){
+        resultText = "Your answers indicate that you could be well suited to a role which is a little technical but mostly allows you to demonstrate a lot of creativity and collaborate with others. Careers like Graphic Design, Technical Support, IT Analyst, QA, Account Manager etc. could await you. Get started on your Tech journey today!";
+    }
+    else if(score>0 && score<40){
+        resultText = "Your answers indicate that you could be well suited to a role which is involves interaction with a lot of different stakholders and also allows for creativity and doesn't require too much technical knowledge. Careers like Project Management, Software sales, customer support, content writing etc could await you. Get started on your Tech journey today!";
     }
     else{
         resultText = "Error compiling results";
